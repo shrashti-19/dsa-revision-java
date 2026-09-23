106730e60b5e43b38e1af9004ad73b247e67821d
shrashti-19 <shrashtidwivedi91@gmail.com>
2026-09-24 00:00:01 +0530

    missing number using xor approach

--- Arrays.java/MoveZeroes.java
+++ Arrays.java/MoveZeroes.java
@@ -1,18 +1,40 @@
+// class Solution {
+//     public void moveZeroes(int[] nums) {
+//         int left = 0;
+//         for(int right=0 ; right<nums.length ; right++){
+//             if(nums[right]==0){
+//               //do nothing
+//             }else{
+//                 //nums[right]!=0
+//                 int temp = nums[right];
+//                 nums[right] = nums[left];
+//                 nums[left] = temp;
+
+//                 left++;
+
+//             }
+//         }
+//     }
+// }
+
+// xor approach
+
 class Solution {
-    public void moveZeroes(int[] nums) {
-        int left = 0;
-        for(int right=0 ; right<nums.length ; right++){
-            if(nums[right]==0){
-              //do nothing
-            }else{
-                //nums[right]!=0
-                int temp = nums[right];
-                nums[right] = nums[left];
-                nums[left] = temp;
+    public int missingNumber(int[] nums) {
+        int xor=0;
+        int n= nums.length;
 
-                left++;
-
-            }
+        for(int i=0 ; i<n ; i++){
+            xor^=nums[i];
         }
+
+        int xor1= 0;
+        for(int i=0 ; i<=n ; i++){
+            xor1^=i;
+        }
+
+        int result = xor^xor1;
+
+        return result;
     }
 }
--- Arrays.java/RemoveDuplicate.java
+++ Arrays.java/RemoveDuplicate.java
@@ -1,4 +1,4 @@
-class Solution {
+class RemoveDuplicateArraySolution {
     public int removeDuplicates(int[] nums) {
         int left =0;
         for(int i=1 ; i<nums.length ; i++){
