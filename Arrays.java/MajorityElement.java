12008960a274c4c8b642bfc696a08a5403bba3fd
shrashti-19 <shrashtidwivedi91@gmail.com>
2026-09-24 23:35:56 +0530

    Majority element using Boye Moore's Voting algo

--- Arrays.java/MajorityElement.java
+++ Arrays.java/MajorityElement.java
@@ -1,29 +1,46 @@
 import java.util.*;
 
 public class MajorityElement {
+    // public static int majorityElement(int[] nums) {
+    //     HashMap<Integer,Integer> count = new HashMap<>();
+    //     int n = nums.length;
+
+    //     for(int i=0 ; i<n ; i++){
+    //         int current = nums[i];
+
+    //         //if current already exists
+    //         if(count.containsKey(current)){
+    //             //increase the count
+    //             count.put(nums[i], count.getOrDefault(nums[i],0)+1);
+    //         }else{
+    //             // it doesn't exist already in the HashMap
+    //             count.put(current, 1);
+    //         }
+    //     }
+
+    //     for(Map.Entry<Integer,Integer> entry : count.entrySet()){
+    //         if(entry.getValue()>n/2){
+    //             return entry.getKey();
+    //         }
+    //     }
+
+    //     return -1;
+    // }
+
     public static int majorityElement(int[] nums) {
-        HashMap<Integer,Integer> count = new HashMap<>();
-        int n = nums.length;
+        int candidate = 0;
+        int count = 0;
 
-        for(int i=0 ; i<n ; i++){
-            int current = nums[i];
-
-            //if current already exists
-            if(count.containsKey(current)){
-                //increase the count
-                count.put(nums[i], count.getOrDefault(nums[i],0)+1);
+        for(int i=0 ; i<nums.length ; i++){
+            if(count == 0){
+                candidate = nums[i];
+            }
+            if(candidate == nums[i]){
+                count++;
             }else{
-                // it doesn't exist already in the HashMap
-                count.put(current, 1);
+                count--;
             }
         }
-
-        for(Map.Entry<Integer,Integer> entry : count.entrySet()){
-            if(entry.getValue()>n/2){
-                return entry.getKey();
-            }
-        }
-
-        return -1;
+        return candidate;
     }
 }
